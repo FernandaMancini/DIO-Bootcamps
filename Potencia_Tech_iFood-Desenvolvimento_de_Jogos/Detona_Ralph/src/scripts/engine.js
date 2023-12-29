b0955400881b3db3547e29859aac580db9ea47b9
Fernanda <fernandamancini07@gmail.com>
2023-12-28 22:58:33 -0300

    Adicionando novos arquivos

---
+++ Potencia_Tech_iFood-Desenvolvimento_de_Jogos/Detona_Ralph/src/scripts/engine.js
@@ -0,0 +1,79 @@
+const state = {
+    view: {
+      squares: document.querySelectorAll(".square"),
+      enemy: document.querySelector(".enemy"),
+      timeLeft: document.querySelector("#time-left"),
+      score: document.querySelector("#score"),
+      lives: document.querySelector("#lives"),
+    },
+    values: {
+      gameVelocity: 600,
+      hitPosition: 0,
+      result: 0,
+      curretTime: 60,
+      lives: 3,
+    },
+    actions: {
+      timerId: setInterval(randomSquare, 1000),
+      countDownTimerId: setInterval(countDown, 1000),
+    },
+  };
+  
+  function countDown() {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+  
+    if (state.values.curretTime <= 0) {
+      playSound("game-over.mp3");
+      clearInterval(state.actions.countDownTimerId);
+      clearInterval(state.actions.timerId);
+      alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+  }
+  
+  function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}`);
+    audio.volume = 0.2;
+    audio.play();
+  }
+  
+  function randomSquare() {
+    state.view.squares.forEach((square) => {
+      square.classList.remove("enemy");
+    });
+  
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+  }
+  
+  function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+      square.addEventListener("mousedown", () => {
+        if (square.id === state.values.hitPosition) {
+          state.values.result++;
+          state.view.score.textContent = state.values.result;
+          state.values.hitPosition = null;
+          playSound("hit.m4a");
+        } else {
+          // Se clicar na posição errada, decrementa a vida e verifica se o jogo acabou
+          state.values.lives--;
+          state.view.lives.textContent = state.values.lives;
+          playSound("errou.mp3");
+          if (state.values.lives === 0) {
+            playSound("game-over.mp3");
+            clearInterval(state.actions.countDownTimerId);
+            clearInterval(state.actions.timerId);
+            alert("Game Over! O seu resultado foi: " + state.values.result);
+          }
+        }
+      });
+    });
+  }
+  
+  function initialize() {
+    addListenerHitBox();
+  }
+  
+  initialize();
---
+++ Potencia_Tech_iFood-Desenvolvimento_de_Jogos/Trilha_css-Desafio01/README.md
@@ -0,0 +1,4 @@
+# Desafio 01: Criando sua Primeira Landing Page com HTML e CSS da DIO 🚀
+
+Bem-vindo ao desafio inicial da Trilha de CSS da DIO!🎉
+O objetivo deste desafio é construir uma Landing Page utilizando HTML e CSS, aplicando os fundamentos primordiais do CSS, suas propriedades básicas de estilização, além de explorar as unidades de medidas relativas e absolutas que foram abordadas ao longo da trilha.
